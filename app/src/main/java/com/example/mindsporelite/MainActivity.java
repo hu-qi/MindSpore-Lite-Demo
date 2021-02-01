99075739878d09f92433450b72d2b8ef33263f19
huqi <huqi1008301@163.com>
2021-02-01 11:11:21 +0800

    :tada: init

---
+++ app/src/main/java/com/example/mindsporelite/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mindsporelite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
